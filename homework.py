fdd36c8faf36b211f0299e636b63dbea00216f37
Vladimir Maksimov <maksimovv0ff@yandex.ru>
2022-10-12 11:50:04 +0300

    Add def check_tokens, def check_response, def get_api_answer

--- homework.py
+++ homework.py
@@ -1,11 +1,17 @@
-...
+import os
+from pprint import pprint
+
+import requests
+import time
+
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,63 +26,73 @@ HOMEWORK_STATUSES = {
 
 
 def send_message(bot, message):
-    ...
+    pass
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
 
-    ...
+    homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    return homework_statuses.json()
 
 
 def check_response(response):
 
-    ...
+    homeworks = response.get('homeworks')
+    return homeworks
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
+    # homework_name = ...
+    # homework_status = ...
 
-    ...
+    # ...
 
-    verdict = ...
+    # verdict = ...
 
-    ...
+    # ...
 
-    return f'Изменился статус проверки работы "{homework_name}". {verdict}'
-
+    # return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+    pass
 
 def check_tokens():
-    ...
-
+    '''Проверяет доступность переменных окружения.'''
+    if not PRACTICUM_TOKEN:
+        return False
+    if not TELEGRAM_TOKEN:
+        return False
+    if not TELEGRAM_CHAT_ID:
+        return False
+    return True
 
 def main():
     """Основная логика работы бота."""
 
-    ...
+    # ...
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    # bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    # current_timestamp = int(time.time())
 
-    ...
+    # ...
 
-    while True:
-        try:
-            response = ...
+    # while True:
+        # try:
+            # response = ...
 
-            ...
+            # ...
 
-            current_timestamp = ...
-            time.sleep(RETRY_TIME)
+            # current_timestamp = ...
+            # time.sleep(RETRY_TIME)
 
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
+        # except Exception as error:
+            # message = f'Сбой в работе программы: {error}'
+            # ...
+            # time.sleep(RETRY_TIME)
+        # else:
+            # ...
+    response = get_api_answer(1)
+    pprint(check_response(response))
 
 
 if __name__ == '__main__':
