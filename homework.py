306dbc29764d0db1d9817782708b2bc1a28a18bf
Vladimir Maksimov <maksimovv0ff@yandex.ru>
2022-10-19 23:44:41 +0300

    Add .env.example

--- homework.py
+++ homework.py
@@ -1,104 +1,100 @@
+import logging
 import os
+import time
+from logging.config import fileConfig
 from pprint import pprint
 
 import requests
-import time
-import logging
-
 from dotenv import load_dotenv
-from logging.config import fileConfig
 from telegram import Bot
 
 from exceptions import TokenError
 
-
 load_dotenv()
 
-PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
-TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
 RETRY_TIME = 600
-ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
-HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+ENDPOINT = "https://practicum.yandex.ru/api/user_api/homework_statuses/"
+HEADERS = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
 
 
 HOMEWORK_STATUSES = {
-    'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
-    'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    "approved": "Работа проверена: ревьюеру всё понравилось. Ура!",
+    "reviewing": "Работа взята на проверку ревьюером.",
+    "rejected": "Работа проверена: у ревьюера есть замечания.",
 }
 
 
-fileConfig('logging_config.ini')
+fileConfig("logging_config.ini")
 logger = logging.getLogger(__name__)
 
 
-
-
-
-
 def send_message(bot, message):
-    '''Отправка сообщения в Телеграм.'''
+    """Отправка сообщения в Телеграм."""
     bot.send_message(TELEGRAM_CHAT_ID, message)
-    logger.info('Telegram message sent')
+    logger.info("Telegram message sent")
 
 
 def get_api_answer(current_timestamp):
+    """Запрос к эндпоинту API."""
     timestamp = current_timestamp or int(time.time())
-    params = {'from_date': timestamp}
+    params = {"from_date": timestamp}
 
     homework_statuses = requests.get(ENDPOINT, headers=HEADERS, params=params)
+
     if homework_statuses.status_code != 200:
-        raise TypeError('ПЕРЕПИШИ ОШИБКУ, тут говно со статусом') # ToDo Подумай как исправить ошибку
-    logger.info('Worked out function get_api_answer')
+        logger.error("API not available")
+        homework_statuses.raise_for_status()
+
+    logger.info("Worked out function get_api_answer")
     return homework_statuses.json()
 
 
 def check_response(response):
+    """Проверяет API на корректность."""
     if isinstance(response, dict):
-        homeworks = response.get('homeworks')
+        homeworks = response.get("homeworks")
         if len(homeworks) != 0:
-            logger.info('Worked out function check_response')
+            logger.info("Worked out function check_response")
             return homeworks[0]
-        logger.info('Worked out function check_response (no homework)')
+        logger.info("Worked out function check_response (no homework)")
         return homeworks
-    raise TypeError('В функцию check_response был передан не словарь')
+    raise TypeError("В функцию check_response был передан не словарь")
 
 
 def parse_status(homework):
+    """Извлекает информацию о конкретной домашней работе."""
     if len(homework) != 0:
         if isinstance(homework, dict):
-            homework_name = homework['homework_name']
-
-            homework_status = homework.get('status')
-
-            # ...
-
+            homework_name = homework.get("homework_name")
+            homework_status = homework.get("status")
             verdict = HOMEWORK_STATUSES.get(homework_status)
 
-            # ...
-
-            logger.info('Worked out function parse_status')
+            logger.info("Worked out function parse_status")
             return f'Изменился статус проверки работы "{homework_name}". {verdict}'
         else:
-            raise KeyError('Тут какое-то говно - поправь') # ToDo подумай как поменять ошибку
-    logger.info('Worked out function parse_status (no homework)')
-    return 'За выбранный отрезок времени нет проверенных работ'
+            raise KeyError(
+                "Тут какое-то говно - поправь"
+            )  # ToDo подумай как поменять ошибку
+    logger.info("Worked out function parse_status (no homework)")
+    return "За выбранный отрезок времени нет проверенных работ"
 
 
 def check_tokens():
-    '''Проверяет доступность переменных окружения.'''
+    """Проверяет доступность переменных окружения."""
     if not PRACTICUM_TOKEN:
-        logger.critical('Invalid or unavailable token PRACTICUM_TOKEN')
+        logger.critical("Invalid or unavailable token PRACTICUM_TOKEN")
         return False
     if not TELEGRAM_TOKEN:
-        logger.critical('Invalid or unavailable token TELEGRAM_TOKEN')
+        logger.critical("Invalid or unavailable token TELEGRAM_TOKEN")
         return False
     if not TELEGRAM_CHAT_ID:
-        logger.critical('Invalid or unavailable token TELEGRAM_CHAT_ID')
+        logger.critical("Invalid or unavailable token TELEGRAM_CHAT_ID")
         return False
-    logger.info('Worked out function check_tokens')
+    logger.info("Worked out function check_tokens")
     return True
 
 
@@ -107,7 +103,7 @@ def main():
     if not check_tokens():
         raise TokenError()
     bot = Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = 1 # int(time.time())
+    current_timestamp = 1  # int(time.time())
 
     response = get_api_answer(current_timestamp)
     sample_result = parse_status(check_response(response))
@@ -124,13 +120,12 @@ def main():
             time.sleep(RETRY_TIME)
 
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
+            message = f"Сбой в работе программы: {error}"
             send_message(message)
             time.sleep(RETRY_TIME)
         # else:
-            # ...
+        # ...
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
